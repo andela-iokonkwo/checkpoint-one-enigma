9387a738c907ccde3491d5e6b9d2f092d1689bb9
Ikem Okonkwo <ikem.okonkwo@andela.com>
2016-03-03 22:15:30 +0100

    add 002_key_spec

--- spec/002_key_spec.rb
+++ spec/002_key_spec.rb
@@ -1,1 +1,21 @@
+require 'spec_helper'
 
+describe Enigma::Key do
+  subject { Enigma::Key.new(55190) }
+
+  describe "#a" do
+    it { expect(subject.a).to eq 55 }
+  end
+
+  describe "#b" do
+    it { expect(subject.b).to eq 51 }
+  end
+
+  describe "#c" do
+    it { expect(subject.c).to eq 19 }
+  end
+
+  describe "#d" do
+    it { expect(subject.d).to eq 90 }
+  end
+end
