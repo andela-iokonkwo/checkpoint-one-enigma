c0868f674bcc49aed53deafcc9e27b1faf48166c
Ikem Okonkwo <ikem.okonkwo@andela.com>
2016-03-06 17:59:44 +0100

    modify names of variable and methods in cracker

--- lib/enigma/crack.rb
+++ lib/enigma/crack.rb
@@ -1,15 +1,14 @@
 module Enigma
   class Crack
     def initialize(date, file_name)
-      @file_name = file_name
       file_store = FileStore.new
-      key = Cracker.new(file_name, file_store, date).generate_key
+      key = Cracker.new(file_store, date).generate_key_for(file_name)
       encryption_key = EncryptionKey.new(date, key)
       @engine = Engine.new(encryption_key, file_store)
     end
 
-    def call(output_file_name)
-      @engine.save_and_report @file_name, output_file_name, :-
+    def call(file_name, output_file_name)
+      @engine.save_and_report file_name, output_file_name, :-
     end
   end
 end
--- lib/enigma/cracker.rb
+++ lib/enigma/cracker.rb
@@ -2,76 +2,75 @@ module Enigma
   class Cracker
     include CharacterMapper
 
-    def initialize(file_name, file_store, date)
+    def initialize(file_store, date)
       @file_store = file_store
       @offset = OffSet.new(date)
-      encryption_key(file_name)
     end
 
-    def map_encrypted_to_known_values(file_name)
+    def generate_key_for(file_name)
+      @key = []
+      encryption_key = generate_encryption_key_by(file_name)
+      @key[0] = encryption_key[0] - @offset.a
+      @key[1] = encryption_key[1] - @offset.b
+      @key[2] = encryption_key[2] - @offset.c
+      @key[3] = encryption_key[3] - @offset.d
+      start_generating
+      # require "pry"; binding.pry
+    end
+
+    # private
+
+    def generate_encryption_key_by(file_name)
+      set = map_encrypted_to_known(file_name)
+      encryption_key = []
+      (0..3).each do |index|
+        chars_set = set[index]
+        encryption_key[index] = position_diff(chars_set)
+      end
+      encryption_key
+    end
+
+     def map_encrypted_to_known(file_name)
       text = @file_store.get(file_name)
       known_digit = text.size % 4
       known_digit += 4 if known_digit < 4
       encrypted_chars = text[-known_digit, 4]
       known_chars = "..end.."[-known_digit, 4].split('')
-      encrypted_chars.zip(known_chars)
+      known_chars.zip(encrypted_chars)
     end
 
-    def encryption_key(file_name)
-      context = map_encrypted_to_known_values(file_name)
-      @encryption_key = []
-      (0..3).each do |index|
-        current_set = context[index]
-        @encryption_key[index] = calculate_diff(current_set)
-      end
+    def position_diff(chars_set)
+      character_hash[chars_set[1]] - character_hash[chars_set[0]]
     end
 
-    def calculate_diff(set)
-      character_hash[set[0]] - character_hash[set[1]]
-    end
-
-    def generate_key
-      @normal_keys = []
-      @normal_keys[0] = @encryption_key[0] - @offset.a
-      @normal_keys[1] = @encryption_key[1] - @offset.b
-      @normal_keys[2] = @encryption_key[2] - @offset.c
-      @normal_keys[3] = @encryption_key[3] - @offset.d
-      generate_normal_key
-    end
-
-    def generate_normal_key
-      final_keys = []
+    def start_generating(key = @key)
+      final_key = []
       index = 0
       while index < 3
-        first = make_positive(@normal_keys[index])
-        second = make_positive(@normal_keys[index + 1])
-        final_keys[index], final_keys[index + 1] = match(first, second)
+        first = make_positive(key[index])
+        second = make_positive(key[index + 1])
+        final_key[index], final_key[index + 1] = rotate_to_match(first, second)
         index += 1
       end
-      final_keys.map!(&:to_s)
-      "#{final_keys[0]}#{final_keys[1][1]}#{final_keys[3]}".to_i
+      final_key.map!(&:to_s)
+      "#{final_key[0]}#{final_key[1][1]}#{final_key[3]}".to_i
     end
 
     def make_positive(num)
-      while num < 0
-        num += 39
-      end
-      num
+      return num unless num < 0
+      make_positive(num + 39)
     end
 
-    def match(first, second)
+    def rotate_to_match(first, second)
       original_second = second
-      catch(:found) do
-        while first < 100
-          second = original_second
-          while second < 100
-            throw :found if key_equal(first, second)
-            second += 39
-          end
-          first += 39
+      while first < 100
+        second = original_second
+        while second < 100
+          return [first, second] if key_equal(first, second)
+          second += 39
         end
+        first += 39
       end
-      [first, second]
     end
 
     def key_equal(first, second)
